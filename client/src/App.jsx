0f455ceb26838c4b8e28a127555196169b2888f6
Easha Angali <eashaangalikhatri@gmail.com>
2026-08-28 10:53:53 -0700

    Add beauty analysis routes and fix formatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
@@ -11,28 +12,46 @@ import Checkout from "./pages/Checkout.jsx";
 import OrderSuccess from "./pages/OrderSuccess.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
+
 import AdminLogin from "./pages/AdminLogin.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
 import AdminProducts from "./pages/AdminProducts.jsx";
 import AdminOrders from "./pages/AdminOrders.jsx";
 import AdminReviews from "./pages/AdminReviews.jsx";
+
 import WriteReview from "./pages/WriteReview.jsx";
 import CustomerReviews from "./pages/CustomerReviews.jsx";
+
+import BeautyAnalysis from "./pages/BeautyAnalysis.jsx";
+import SkinAnalysis from "./pages/SkinAnalysis.jsx";
+import HairAnalysis from "./pages/HairAnalysis.jsx";
+
 function App() {
   return (
     <>
       <Navbar />
+
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+
           <Route path="/shop" element={<Shop />} />
-          <Route path="/reviews"element={<CustomerReviews />} />
+          <Route path="/reviews" element={<CustomerReviews />} />
+
           <Route path="/product/:id" element={<ProductDetails />} />
+
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/order-success/:id" element={<OrderSuccess />} />
+
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+
+          {/* BEAUTY ANALYSIS */}
+          <Route path="/analysis" element={<BeautyAnalysis />} />
+          <Route path="/analysis/skin" element={<SkinAnalysis />} />
+          <Route path="/analysis/hair" element={<HairAnalysis />} />
+
           <Route path="/admin/login" element={<AdminLogin />} />
 
           <Route
@@ -43,6 +62,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+
           <Route
             path="/admin/products"
             element={
@@ -51,6 +71,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+
           <Route
             path="/admin/orders"
             element={
@@ -59,6 +80,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+
           <Route
             path="/admin/reviews"
             element={
@@ -67,13 +89,11 @@ function App() {
               </ProtectedRoute>
             }
           />
- 
-          <Route
-  path="/write-review"
-  element={<WriteReview />}
-/>
+
+          <Route path="/write-review" element={<WriteReview />} />
         </Routes>
       </main>
+
       <Footer />
     </>
   );
